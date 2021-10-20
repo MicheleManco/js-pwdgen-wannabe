0aa4b358912ae228e698bb8f4bbc28e6dfb13e3c
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2021-10-20 13:23:07 +0200

    inizializzazione

---
+++ js/myscript.js
@@ -0,0 +1,7 @@
+//Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito. Infine scrivi sulla pagina “La password assegnata è: ” nomecognomecolorepreferito21
+
+// chiedo il nome, cognome, colore
+
+//formulo la password 
+
+//scrivo la password
