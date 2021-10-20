d1cc2adbf06f50eefbadf74bf274868d6c45c9f2
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2021-10-20 15:22:19 +0200

    finito

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,12 @@
 //Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito. Infine scrivi sulla pagina “La password assegnata è: ” nomecognomecolorepreferito21
 
 // chiedo il nome, cognome, colore
+let nome = prompt("inserisci il tuo nome")
+let cognome = prompt("inserisci il tuo cognome")
+let colorePreferito = prompt("inserisci il tuo colore preferito")
 
 //formulo la password 
+let password = nome + cognome + colorePreferito + "21"
 
 //scrivo la password
+document.getElementById("password").innerHTML = "Questa sarà la tua password: " + password
